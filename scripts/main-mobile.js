0c44a3ecb36492f768f2b2c5e13cb540e913c727
Alexandr <AlMi@Mac-mini-Alexandr.local>
2018-04-10 15:51:38 +0300

    change in contact

--- scripts/main-mobile.js
+++ scripts/main-mobile.js
@@ -39,8 +39,6 @@ function startFunctionAnimationOnVisibleObject(object, functionAnimation, param,
     var winHeight = window.innerHeight;
     var winScrollY = window.scrollY;
 
-    console.log(winHeight, winScrollY);
-
     var _object = object;
     var containerOffsetTop = _object.offsetTop;
     var containerHeight = _object.getBoundingClientRect().height;
