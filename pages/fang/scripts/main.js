bccf72bc766f75817fc899ffe3505617ecdb3d21
Alexandr <AlMi@Mac-mini-Alexandr.local>
2018-04-12 07:44:32 +0300

    fix js, aff favicon

--- pages/fang/scripts/main.js
+++ pages/fang/scripts/main.js
@@ -1,18 +1,14 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
-window.addEventListener('load', function () {
-  var navBtn = document.querySelector('.navigation__btn');
-  var nav = document.querySelector('.navigation__list-container');
-  window.addEventListener('click', function (e) {
-    console.log(e.currentTarget);
-  });
-  console.log(navBtn);
-  navBtn.addEventListener('click', function () {
-    console.log(1);
-    nav.classList.toggle('active');
-  });
+var navBtn = document.querySelector('.navigation__btn');
+var nav = document.querySelector('.navigation__list-container');
+
+navBtn.addEventListener('click', function () {
+  nav.classList.toggle('active');
 });
+
+window.addEventListener('load', function () {});
 
 },{}]},{},[1])
 
